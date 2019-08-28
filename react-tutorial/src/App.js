80c8e5529e630cdb1c849e5326f6963d39128a02
venuschau <Venus.Chau@pccw.com>
2019-08-28 16:50:12 +0800

    review

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import Table from './Table';
 import Form from './Form';
 import './App.css';
@@ -29,7 +28,7 @@ class App extends Component {
 
   removeCharacter = index => {
     const { characters } = this.state
-    
+
     this.setState({
       characters: characters.filter((character, i) => {
         return i !== index
@@ -62,24 +61,11 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
         <div className="container">
           <Table characterData={characters} removeCharacter={this.removeCharacter} />
-          <Form handleSubmit={this.handleSubmit}/>
+          <Form handleSubmit={this.handleSubmit} />
         </div>
+        
       </div>
     );
   }
